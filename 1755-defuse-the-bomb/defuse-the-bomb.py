a089d30ee4b45f55ff5d42c24a5d7d1d26bdde75
Sri Saran P <srisaranp123@gmail.com>
2026-06-26 20:52:52 +0530

    Time: 23 ms (5.60%) | Memory: 19.5 MB (8.53%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.py
@@ -0,0 +1,26 @@
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        res=[]
+        n=len(code)
+        for i in range(n):
+            x=0
+            if k>0:index=i+1
+            else:index=i-1
+            s=0
+            temp=[]
+            while x<abs(k):
+                if index<0:
+                    index+=n
+                if index==n:index%=n
+
+                s+=code[index]
+                temp.append(code[index])
+                x+=1
+                if k>0:
+                    index+=1
+                else:
+                    index-=1
+            print(temp)
+            res.append(s)
+        print(res)
+        return res
